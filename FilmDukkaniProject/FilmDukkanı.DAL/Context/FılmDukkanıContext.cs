0faf2dbe8c84ce219dfa4471d844205449e06041
Nayir06 <129368367+Nayir06@users.noreply.github.com>
2023-07-07 21:31:40 +0300

    .

---
+++ FilmDukkaniProject/FilmDukkanı.DAL/Context/FılmDukkanıContext.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmDukkanı.DAL.Context
+{
+    internal class FılmDukkanıContext:IdentityDbContext
+    {
+
+
+
+        public DbSet<Category>
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("server=DESKTOP-TQ757R7\\hasan;database=FılmDukkanıDB;uid=sa;pwd=123");
+            }
+        }
+    }
+}
